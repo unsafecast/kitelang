cc2bae99e1cba9493c0a02142670e20d2e92a140
unsafecast <unsafecast@protonmail.com>
2021-12-06 14:16:06 +0200

    fixed the colors in the kite_error stuff

--- kite_error.c
+++ kite_error.c
@@ -6,7 +6,7 @@ void kite_error(const char* fmt, ...)
 {
     va_list args;
     va_start(args, fmt);
-    fprintf(stderr, "\033[31merror: \033[0m");
+    fprintf(stderr, "\033[31;1merror: \033[0m");
     vfprintf(stderr, fmt, args);
     va_end(args);
 }
@@ -15,7 +15,7 @@ void kite_warning(const char* fmt, ...)
 {
     va_list args;
     va_start(args, fmt);
-    fprintf(stderr, "\033[33mwarning: \033[0m");
+    fprintf(stderr, "\033[33;1mwarning: \033[0m");
     vfprintf(stderr, fmt, args);
     va_end(args);
 }
@@ -24,7 +24,7 @@ void kite_hint(const char* fmt, ...)
 {
     va_list args;
     va_start(args, fmt);
-    fprintf(stderr, "\033[36mhint: \033[0m");
+    fprintf(stderr, "\033[36;1mhint: \033[0m");
     vfprintf(stderr, fmt, args);
     va_end(args);
 }
